1250fee5d72e962f8989caa62d36c04394177856
f0s3 <power.aerozoll@gmail.com>
2019-12-31 01:31:59 +0200

    Implement ContactList using OOP aproach

--- src/ContactList.java
+++ src/ContactList.java
@@ -1,27 +1,31 @@
-public class ContactList {
-    String[][] contacts = {};
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 
-    public String[][] getContacts() {
+public class ContactList {
+    ArrayList<Contact> contacts = new ArrayList<>();
+
+    public ArrayList<Contact> getContacts() {
         return this.contacts;
     }
 
     public void printContacts() {
-        for (int i = 0;i < getContacts().length;i++) {
-            for (int j = 0;j < getContacts()[i].length;j++) {
-                // name - number
-//                       0                  1
-//               [ [name1, name2], [number1, number2] ]
-
-                System.out.println(getContacts()[i] + " - " + getContacts()[i + 1][i]);
-            }
-        }
-
-        for (i;i < getContacts()) {
-
-        }
-
-        for (j;j < getContacts()[]) {}
-
+        getContacts().forEach(element -> System.out.println(element.getName() + " - " + element.getNumber()));
     }
 
+    public void addContact(Contact contact) {
+        getContacts().add(contact);
+    }
+
+    public void removeContact(Contact contact) {
+        getContacts().remove(contact);
+    }
+
+    public void editContact(Contact oldContact, Contact newContact) {
+        Field[] fields = Contact.class.getDeclaredFields();
+        for (int  i = 0;i < fields.length;i++) {
+            int pos = getContacts().indexOf(oldContact); // get old contacts position
+            removeContact(oldContact); // remove old contact in position `pos`
+            getContacts().add(pos, newContact); // add new contact on position of old contact
+        }
+    }
 }
